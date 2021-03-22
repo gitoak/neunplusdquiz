8aadecee24fe657683d812261053949a1dd57a8f
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2021-03-22 12:16:50 +0100

    Update viewer.tsx

--- src/components/pages/Quizviewer/viewer.tsx
+++ src/components/pages/Quizviewer/viewer.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState, useCallback } from 'react'
-import { getQuiz, resume } from '../../../redux/actions/redux.actions.Quiz'
+import { Redirect } from 'react-router'
+import { getQuiz, resume, endGame } from '../../../redux/actions/redux.actions.Quiz'
 import store from '../../../redux/store/redux.store.configureStore'
 import IPage from '../../../types/pages.IPage'
 import IDQuiz from '../../../types/quizes.IDQuiz'
 import IMQuiz from '../../../types/quizes.IMQuiz'
+import { END_GAME } from '../../../types/redux.IActions'
 import IState from '../../../types/redux.IState'
 import dquizes from '../../molecules/dquizes/quizes'
 import mquizes from '../../molecules/mquizes/quizes'
@@ -16,7 +18,8 @@ const Viewer: React.FC<IPage> = () => {
     const [quizType, setQuizType] = useState<'m' | 'd'>()
     const [state, setState] = useState<IState>()
     const [current, setCurrent] = useState<number>()
-    const [corecctly, setCorecctly] = useState<boolean>()
+    const [corecctly, setCorecctly] = useState<any>()
+    const [reset, setReset] = useState<boolean>()
 
     const update = useCallback(() => {
         setState(store.getState())
@@ -24,9 +27,32 @@ const Viewer: React.FC<IPage> = () => {
 
     store.subscribe(() => update())
 
+    const nextQuiz = () => {
+        if (current !== undefined) {
+            if (corecctly !== undefined) {
+                store.dispatch(resume('m', current, corecctly))
+            } else {
+                store.dispatch(resume('m', current, false))
+            }
+        }
+        setReset(true)
+        store.dispatch(getQuiz('m'))
+        window.location.reload()
+    }
+
+
     useEffect(() => {
         update();
-        console.log(corecctly)
+        // Bessere Anzeige dringend nötig
+        let rounds = state?.round
+        if (rounds !== undefined && rounds > 9) {
+            store.dispatch(endGame())
+            let lstate = localStorage.getItem("state")
+            if (lstate) {var score = parseInt(JSON.parse(lstate).score)} else {var score = 0}
+            alert("Runde: " + rounds + " Punkte: " + score + " | Unvollständig")
+            localStorage.removeItem("state")
+            window.location.reload()
+        }
         if (state?.current) {
             setCurrent(state.current.id)
             switch (state.current.type) {
@@ -55,10 +81,11 @@ const Viewer: React.FC<IPage> = () => {
                     d={mquiz.d}
                     answer={mquiz.answer}
                     solution={mquiz.solution}
-                    isCorrectly={(correctly: boolean) => setCorecctly(corecctly)}
+                    isCorrectly={(correctly: boolean) => setCorecctly(correctly)}
+                    doReset={() => (reset)}
                 ></PopupQuiz>
-                {corecctly !== undefined ? (
-                    <button onClick={() => store.dispatch(resume('m', current, corecctly))}>
+                {corecctly !== undefined && corecctly !== null ? (
+                    <button onClick={() => nextQuiz()}>
                     Weiter
                     </button>
                 ) : (
